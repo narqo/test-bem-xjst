882619b6513d471217e427782473334fd0eb0bb7
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-10-07 02:10:46 +0400

    index updated

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 var run = require('./lib/runner').run,
-    template = require('./templates/simple-block');
+    data = {},
+    template = null;
 
-console.log(run(template, { block : 'b1' }));
+// 1. simple-block
+
+template = require('./templates/simple-block');
+data = {block : 'b1'};
+
+console.log(run(template, data));
+
+// 2. simple-mods
+
+template = require('./templates/simple-mods');
+data = {block : 'b1', mods : {type : 'simple'}};
+
+console.log(run(template, data));
+
+// 3. simple-elem
+
+template = require('./templates/simple-elem');
+data = {block : 'b1', elem : 'e1', content : 'my-content'};
+
+console.log(run(template, data));
+
